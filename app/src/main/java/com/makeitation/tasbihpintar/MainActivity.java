40f29a37de45da9f177839ac4e17b4540bb0875c
toufani1515 <toufani1515@gmail.com>
2020-03-28 02:46:58 +0800

    penambahan fitur TapTargetView pada MainActivity dan Animation Recycler View pada Acitivy Panduan

---
+++ app/src/main/java/com/makeitation/tasbihpintar/MainActivity.java
@@ -0,0 +1,300 @@
+package com.makeitation.tasbihpintar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetSequence;
+import com.getkeepsafe.taptargetview.TapTargetView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pusher.pushnotifications.PushNotifications;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNav;
+    private int counter = 0;
+    private int initialCounter = 0;
+    private boolean getar = false;
+    private boolean refrences = false;
+    FloatingActionButton floatingActionButton ;
+    TextView number, namaDzikir;
+    RelativeLayout lay3, lay2;
+    Vibrator vibrator;
+
+    // favorite refrence : https://github.com/wasabeef/awesome-android-ui
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_card));
+        changeFontActionBar();
+
+        // cloud massaging
+	    // dokumentasi : https://pusher.com/tutorials/push-notifications-android
+        PushNotifications.start(getApplicationContext(), "5e5cb362-53dd-4170-9b88-c4f1637f70d1");
+        PushNotifications.addDeviceInterest("hello");
+        // cloud massaging
+
+        // Tap Target View
+
+        /*
+        TOOLBAR
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_card));
+        */
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean booting = sharedPreferences.contains("booting");
+        Log.d("KONDISI", String.valueOf(booting));
+        if (booting){
+            // do something
+        } else {
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("booting", "pertama_kali");
+            editor.commit();
+
+            Typeface typeface = getResources().getFont(R.font.myfont); // ganti font title bar
+
+            // efek tap target untuk perkenalan fitur di aplikasi saat aplikasi pertama kali diinstal
+            // https://github.com/KeepSafe/TapTargetView
+            new TapTargetSequence(this)
+                    .targets(TapTarget.forView(findViewById(R.id.fab),"Tombol Reset", "Klik tombol ini untuk mereset counter ke nilai 0")
+                                    .tintTarget(false)
+                                    .descriptionTypeface(typeface)
+                                    .outerCircleColor(R.color.floatingColor),
+                            TapTarget.forView(findViewById(R.id.play), "Tombol Play", "Klik tombol ini untuk memulai perhitungan dzikir")
+                                    .tintTarget(false)
+                                    .descriptionTypeface(typeface)
+                                    .targetRadius(70)
+                                    .outerCircleColor(R.color.colorPrimary),
+                            TapTarget.forView(findViewById(R.id.card_view_2), "Link Bacaan", "Dapatkan panduan bacaan dzikir yang up to date")
+                                    .tintTarget(false)
+                                    .descriptionTypeface(typeface)
+                                    .outerCircleColor(R.color.panduan_bacaan))
+                    .listener(new TapTargetSequence.Listener() {
+                @Override
+                public void onSequenceFinish() {
+                    Toast.makeText(MainActivity.this, "Selamat berdzikir :)", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onSequenceStep(TapTarget lastTarget, boolean targetClicked) {
+
+                }
+
+                @Override
+                public void onSequenceCanceled(TapTarget lastTarget) {
+
+                }
+            }).start();
+
+        }
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        number = findViewById(R.id.number);
+        namaDzikir = findViewById(R.id.pujian);
+
+        /*
+        Atur nilai awalan nama zikir dan nilai counternya
+         */
+        final String firstVal = String.valueOf(initialCounter);
+        number.setText(firstVal);
+        namaDzikir.setText("Tasbih (سبحان الله)");
+
+        lay3 = findViewById(R.id.lay3);
+        lay3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter += 1;
+                if (counter == 33){
+                    vibration(vibrator, 500);
+                    number.setText(String.valueOf(33));
+
+                } else if (counter == 34){
+                    namaDzikir.setText("Tahmid (الحمدلله)");
+                    number.setText(String.valueOf(34));
+
+                }
+                else if (counter == 66){
+                    vibration(vibrator, 500);
+                    number.setText(String.valueOf(66));
+                } else if (counter == 67){
+                    namaDzikir.setText("Takbir (الله أكبر)");
+                    number.setText(String.valueOf(67));
+
+                }
+
+                else if (counter == 99){
+                    number.setText(String.valueOf(99));
+                    vibration(vibrator, 500);
+                } else if (counter == 100){
+                    namaDzikir.setText("Tahlil");
+                    number.setText(String.valueOf(counter));
+                } else if (counter == 101){
+                    counter = 0;
+                    vibration(vibrator, 700);
+                    number.setText(String.valueOf(counter));
+                }
+
+                else {
+                    if (getar == true){
+                        vibration(vibrator, 55);
+                    number.setText(String.valueOf(counter));
+
+                    } else {
+                            number.setText(String.valueOf(counter));
+                        }
+                }
+            }
+        });
+
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = 0;
+                number.setText(String.valueOf(counter));
+                namaDzikir.setText("Tasbih (سبحانالله)");
+                vibration(vibrator, 250);
+
+            }
+        });
+
+        lay2 = findViewById(R.id.lay2);
+        lay2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent panduan = new Intent(MainActivity.this, Panduan.class);
+                startActivity(panduan);
+            }
+        });
+
+/*
+        Fragment Bottom navigation
+
+        mBottomNav = findViewById(R.id.navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_favorites:
+                        Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+ */
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        MenuItem item = menu.findItem(R.id.switchId);
+        item.setActionView(R.layout.layout_switch);
+        Switch switchBtn = item.getActionView().findViewById(R.id.switchAB);
+        switchBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    getar = true;
+                    Toast.makeText(MainActivity.this, "Mode Getar ON", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    getar = false;
+                    Toast.makeText(MainActivity.this, "Mode Getar OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about:
+                AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+                alBuilder.setTitle("About")
+                        .setMessage("Tasbih Pintar adalah aplikasi yang memudahkan kita semua sebagai umat muslim untuk berdzikir. Dengan adanya aplikasi ini, diharapkan semua yang menggunakan ini bisa meningkatkan ibadah kepada Allah SWT. \n\n*Dibuat dengan cinta dan iman*\nCreated by Hamba Allah Dev")
+                        .setCancelable(false)
+                        .setIcon(R.drawable.info)
+                        .setPositiveButton("Tutup", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alBuilder.create();
+                alBuilder.show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void vibration(Vibrator vibrator, int volume){
+        vibrator.vibrate(VibrationEffect.createOneShot(volume, VibrationEffect.DEFAULT_AMPLITUDE));
+    }
+
+    public void changeFontActionBar(){
+        ActionBar ab = getSupportActionBar();
+        // Create a TextView programmatically.
+        TextView tv = new TextView(getApplicationContext());
+        // Create a LayoutParams for TextView
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, // Width of TextView
+                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+        // Apply the layout parameters to TextView widget
+        tv.setLayoutParams(lp);
+        // Set text to display in TextView
+        tv.setText(ab.getTitle()); // ActionBar title text
+        // Set the text color of TextView to black
+        tv.setTextColor(Color.WHITE);
+        tv.setTextSize(20);
+        // Set the monospace font for TextView text
+        // This will change ActionBar title text font
+        Typeface typeface = getResources().getFont(R.font.myfont);
+        tv.setTypeface(typeface);
+        // Set the ActionBar display option
+        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        // Finally, set the newly created TextView as ActionBar custom view
+        ab.setCustomView(tv);
+    }
+
+}
---
+++ app/src/main/java/com/makeitation/tasbihpintar/Panduan.java
@@ -0,0 +1,189 @@
+package com.makeitation.tasbihpintar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import me.zhanghai.android.materialprogressbar.IndeterminateHorizontalProgressDrawable;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.mlsdev.animatedrv.AnimatedRecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Panduan extends AppCompatActivity {
+
+    ArrayList<DataController> list = new ArrayList<>(); // Alokasikan memori untuk simpan list
+    AnimatedRecyclerView recycle;
+    DataAdapter dataAdapter;
+    SwipeRefreshLayout swipeRefreshLayout;
+    ProgressBar progressBar;
+    boolean koneksi = false;
+    String JSON_URL = "https://tasbih-pintar-backend-server.herokuapp.com/api";
+    ProgressBar progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panduan);
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.refresh_list);
+        progress = findViewById(R.id.progress);
+        recycle = findViewById(R.id.recview);
+        recycle.setHasFixedSize(true);
+        changeFontActionBar();
+
+        getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_card));
+
+
+        // ANIMASI DOWN TO UP
+        // https://github.com/MLSDev/AnimatedRecyclerView
+        AnimatedRecyclerView recyclerView = new AnimatedRecyclerView.Builder(this)
+                .orientation(LinearLayoutManager.VERTICAL)
+                .layoutManagerType(AnimatedRecyclerView.LayoutManagerType.LINEAR)
+                .animation(R.anim.layout_animation_from_bottom)
+                .animationDuration(600)
+                .reverse(false)
+                .build();
+        // ANIMASI DOWN TO UP
+
+        getSupportActionBar().setTitle("Panduan Bacaan Lengkap");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        if (isConnected){
+            Log.i("INTERNET : ", String.valueOf(isConnected));
+            koneksi = true;
+            progress.setVisibility(View.VISIBLE);
+            getDetailJson();
+
+
+        } else {
+            koneksi = false;
+            ContextThemeWrapper ctw = new ContextThemeWrapper(Panduan.this, R.style.MyAlert);
+            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ctw);
+            alertDialogBuilder.setTitle("No internet connection");
+            alertDialogBuilder.setMessage("Check your internet connection or try again");
+            alertDialogBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+            alertDialogBuilder.show();
+
+        }
+
+        SwipeRefreshLayout.OnRefreshListener refresh = new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                finish();
+                overridePendingTransition(0, 0);
+                startActivity(getIntent());
+                overridePendingTransition(0, 0);
+                }
+        };
+        swipeRefreshLayout.setOnRefreshListener(refresh);
+
+    }
+
+    private void getDetailJson(){
+        StringRequest stringRequest = new StringRequest(JSON_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    JSONArray jsonArray = obj.getJSONArray("data");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        DataController data = new DataController();
+                        data.setGambar(jsonObject.getString("gambar"));
+                        data.setNamaDzikir(jsonObject.getString("nama_dzikir"));
+                        data.setKeterangan(jsonObject.getString("keterangan"));
+                        data.setNumberItem(i+1);
+                        list.add(data);
+                        viewResult(list);
+
+                        progress.setVisibility(View.INVISIBLE);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void viewResult(ArrayList<DataController> list){
+        recycle.setLayoutManager(new GridLayoutManager(this,1));
+        DataAdapter dataAdapter = new DataAdapter(list);
+        recycle.setAdapter(dataAdapter);
+        dataAdapter.notifyDataSetChanged();
+        recycle.scheduleLayoutAnimation();
+    }
+
+    public void changeFontActionBar(){
+        ActionBar ab = getSupportActionBar();
+        // Create a TextView programmatically.
+        TextView tv = new TextView(getApplicationContext());
+        // Create a LayoutParams for TextView
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, // Width of TextView
+                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+        // Apply the layout parameters to TextView widget
+        tv.setLayoutParams(lp);
+        // Set text to display in TextView
+        tv.setText(ab.getTitle()); // ActionBar title text
+        // Set the text color of TextView to black
+        tv.setTextColor(Color.WHITE);
+        tv.setTextSize(20);
+        // Set the monospace font for TextView text
+        // This will change ActionBar title text font
+        Typeface typeface = getResources().getFont(R.font.myfont);
+        tv.setTypeface(typeface);
+        // Set the ActionBar display option
+        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        // Finally, set the newly created TextView as ActionBar custom view
+        ab.setCustomView(tv);
+    }
+
+
+}
